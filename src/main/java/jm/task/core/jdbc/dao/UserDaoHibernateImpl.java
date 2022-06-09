0f4742c195d9cb5072f222a6ea7e5ef4a7d00a05
korochev <myver33@gmail.com>
2022-06-09 14:47:53 +0300

    INTELLIJ_IDEA//2022.1//20:40

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -15,6 +15,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
             String SQL = "CREATE TABLE IF NOT EXISTS pp_11_users " +
                     "(id INTEGER AUTO_INCREMENT, " +
@@ -22,58 +23,85 @@ public class UserDaoHibernateImpl implements UserDao {
                     " lastname VARCHAR (50) NOT NULL, " +
                     " age INTEGER NOT NULL, " +
                     " PRIMARY KEY (id))";
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             session.createSQLQuery(SQL).executeUpdate();
             tr.commit();
             System.out.println("Table successfully created...");
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS pp_11_users").executeUpdate();
             tr.commit();
             System.out.println("Table successfully removed...");
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             session.save(new User(name, lastName, age));
             tr.commit();
             System.out.println("The user whose name - " + name + " has been added to the database.");
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             User itemFromDb = session.get(User.class, id);
             session.remove(itemFromDb);
             tr.commit();
             System.out.println("Remove record.");
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction tr = null;
+        List<User> list;
         try (Session session = Util.getSessionFactory().openSession()) {
-            return session.createQuery("From User").list();
+            tr = session.beginTransaction();
+            list = session.createQuery("From User").list();
+            tr.commit();
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction tr = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction tr = session.beginTransaction();
+            tr = session.beginTransaction();
             session.createQuery("delete from jm.task.core.jdbc.model.User").executeUpdate();
             tr.commit();
             System.out.println("Remove all records.");
+        } catch (Exception e) {
+            if (tr != null) tr.rollback();
+            throw e;
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
@@ -8,7 +9,7 @@ import java.sql.Connection;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
     public void createUsersTable() {
 
         userDao.createUsersTable();
